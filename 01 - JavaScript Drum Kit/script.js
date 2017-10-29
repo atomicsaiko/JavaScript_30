5b0edb0d35e28d5b5dcc81b36cf73be285ed656e
atomicsaiko <atomicsaiko@gmail.com>
2017-10-29 10:52:57 +0100

    Play instrument on click

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -2,10 +2,25 @@ console.log(document.getElementsByClassName("key"))
 
 var key = document.getElementsByClassName("key")
 
-for(i = 0; i < key.length; i++) {
+for(let i = 0; i < key.length; i++) {
   key[i].addEventListener("click", whichKey)
 }
 
 function whichKey() {
-  console.log(this.dataset.instrument)
+  let instrument = this.dataset.instrument
+  console.log("Clicked:", this.dataset.instrument)
+  playAudio(instrument)
+}
+
+function playAudio(instrument) {
+  if (instrument === "cello") {
+    let cello = document.querySelectorAll("audio[data-instrument='cello']").item(0)
+    cello.play()
+  } else if (instrument === "flute") {
+    let flute = document.querySelectorAll("audio[data-instrument='flute']").item(0)
+    flute.play()
+  } else {
+    let trumpet = document.querySelectorAll("audio[data-instrument='trumpet']").item(0)
+    trumpet.play()
+  }
 }
