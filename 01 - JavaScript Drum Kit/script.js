8ef2f00cad9330715cba2c5766dee23cc42cbc8f
atomicsaiko <atomicsaiko@gmail.com>
2017-10-29 09:46:09 +0100

    Add function that shows which key is clicked

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -3,5 +3,9 @@ console.log(document.getElementsByClassName("key"))
 var key = document.getElementsByClassName("key")
 
 for(i = 0; i < key.length; i++) {
-  key[i].addEventListener("click", () => console.log("Click"))
+  key[i].addEventListener("click", whichKey)
+}
+
+function whichKey() {
+  console.log(this.dataset.instrument)
 }
